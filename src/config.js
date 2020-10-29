da7eac0e8fcf4fc04baf5175cec1ef3fd576d90b
Tomer Levi <18260079+levitomer@users.noreply.github.com>
2020-10-29 20:53:04 +0200

    AWS Amplify configuration

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-attachments-and-uploads"
+    },
+    apiGateway: {
+      REGION: "east",
+      URL: "https://99k3ma0et4.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_cVBHby26k",
+      APP_CLIENT_ID: "1e270etkcak35cg99r5rlufkef",
+      IDENTITY_POOL_ID: "us-east-1:56fbf642-5641-4dea-a755-4cb6aef385ef"
+    }
+  };

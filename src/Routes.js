06f40fc1155890b7d508617a3da667721bec28e9
Tomer Levi <18260079+levitomer@users.noreply.github.com>
2020-11-05 16:51:47 +0200

    Securing routes and redirections

--- src/Routes.js
+++ src/Routes.js
@@ -3,31 +3,34 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './containers/Home';
 import Login from './containers/Login';
 import Signup from './containers/Signup';
-import NewNote from "./containers/NewNote";
-import Notes from "./containers/Notes";
-import Settings from "./containers/Settings";
+import NewNote from './containers/NewNote';
+import Notes from './containers/Notes';
+import Settings from './containers/Settings';
 import NotFound from './containers/NotFound';
+import AuthenticatedRoute from './components/AuthenticatedRoute';
+import UnauthenticatedRoute from './components/UnauthenticatedRoute';
+
 export default function Routes() {
     return (
         <Switch>
             <Route exact path="/">
                 <Home />
             </Route>
-            <Route exact path="/login">
+            <UnauthenticatedRoute exact path="/login">
                 <Login />
-            </Route>
-            <Route exact path="/signup">
+            </UnauthenticatedRoute>
+            <UnauthenticatedRoute exact path="/signup">
                 <Signup />
-            </Route>
-            <Route exact path="/settings">
-              <Settings />
-            </Route>
-            <Route exact path="/notes/new">
+            </UnauthenticatedRoute>
+            <AuthenticatedRoute exact path="/settings">
+                <Settings />
+            </AuthenticatedRoute>
+            <AuthenticatedRoute exact path="/notes/new">
                 <NewNote />
-            </Route>
-            <Route exact path="/notes/:id">
-              <Notes />
-            </Route>
+            </AuthenticatedRoute>
+            <AuthenticatedRoute exact path="/notes/:id">
+                <Notes />
+            </AuthenticatedRoute>
             {/* Finally, catch all unmatched routes */}
             <Route>
                 <NotFound />
